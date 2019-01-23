cc6cdb86c5ff8916ea2dfabed25eee85b4501f12
sinkaroid <bibsindra@gmail.com>
2019-01-24 01:13:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,66 @@
-soon
+<title>Hitomi.la Stealer mirror</title>
+<link rel="shortcut icon" href="https://4.bp.blogspot.com/-DweioerHPNg/XEAf4rEqqqI/AAAAAAAABLc/d_BTxB3uqro1gxaU-iSgK0Apu3YA7pMfgCLcBGAs/s1600/slemanb0ys-ico.jpg" type="image/x-icon">
+<meta name='author' content='Sinkaroid Stupidc0de'>
+<link href="http://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet" type="text/css">
+
+
+<style>
+
+.subbtn {background:#b70505;color:white;border: 1px solid #000; padding:6px 6px 6px 6px;}
+
+
+.bordergaya{
+			background:black;
+			color:white;
+			margin:0 10px;
+			font-family:Ubuntu Mono;
+			font-size:16px;
+			border:2px solid #2d2b2b;
+	
+		}
+		.bordergaya:hover{
+			background:#2d2b2b;
+			color:white;
+			margin:0 10px;
+			font-family:Ubuntu Mono;
+			font-size:16px;
+			border:2px solid crimson;
+		}
+	
+
+.image {
+    width: 400px;
+    height: 350px;
+    -webkit-animation:spin 8s linear infinite;
+    -moz-animation:spin 8s linear infinite;
+    animation:spin 5s linear infinite;
+}
+@-moz-keyframes spin { 100% { -moz-transform: rotate(-360deg); } }
+@-webkit-keyframes spin { 100% { -webkit-transform: rotate(-360deg); } }
+@keyframes spin { 100% { -webkit-transform: rotate(-360deg); transform:rotate(-360deg); } }
+</style>
+
+<center><font face="Ubuntu" size="5" color="black"><b>[!] Hitomi.la Mirror [!]<b/></font></center>
+<br>
+<body bgcolor ="white">
+<center><font face="Ubuntu" size="4" color="black"><b><i>a sec till complete.. then, go put in Hitomi.la grabber</i><b/></font></center>
+<center><a href="grab.php" rel="nofollow" target="_blank"><font face="Ubuntu" size="4" color="red"><b>[ grab here ]<b/></font></a><center>
+<p>
+<center>
+<?php
+for ($i= 1; $i <= 24	; $i++) ///IMPORTANT ,check the folder, see how many pictures there are,  change the variable [24] to max numb in pururin.io gallery
+{
+echo" <img width='220' height='350' src=";
+/**geturl**/ echo "https://ba.hitomi.la/galleries"; ///hentaifox are different afterall, check your url seriously, may can [i/i2] subdomain galleries then look the folder after subdomain 'i/i2' it can be located in [001/002]
+echo "/1142305/"; ///put code here 
+echo $i; echo".png"; ///check nh folder first, if png format just change it
+echo ">";
+}
+
+
+
+?>
+<p>
+<center><font face="Ubuntu" size="5" color="green"><b>[!] Hitomi.la Stealer [!]<b/></font></center>
+<br>
+<center><font face="Ubuntu" size="3" color="green"><b><i>(c) 2019 coded by sinkaroid ~penjagabisu@outlook.com</i><b/></font></center>
